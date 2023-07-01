4b1b174d6f6bebf17b692487732d5b9995e10d77
Marco Guevara <marco.algv@gmail.com>
2023-07-01 15:42:57 -0600

    Initial Commit

---
+++ PokedexV2/PokedexV2App.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexV2App.swift
+//  PokedexV2
+//
+//  Created by Marco Guevara on 1/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
